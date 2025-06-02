35b8074f046c719a848244b79748f797aee80b5f
Umar <umareht@gmail.com>
2025-06-02 14:02:19 +0500

    started currency-convertor app

---
+++ currency-convertor/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-3xl text-orange-500 font-semibold'>Currency Convertor App</h1>
+    </>
+  )
+}
+
+export default App
